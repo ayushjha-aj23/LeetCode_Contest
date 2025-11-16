d7293d76136be0be54dff8bc5e19ad3eb91d478b
Ayush Jha <aj23.workspace@gmail.com>
2025-11-16 16:04:43 +0530

    Enhance comments for minLengthAfterRemovals method
    Added comments to explain the logic behind the removal of balanced substrings and the calculation of the remaining string length.

--- Weekly_0476/02_MinStringLenAfterBalancedRemoval.java
+++ Weekly_0476/02_MinStringLenAfterBalancedRemoval.java
@@ -1,8 +1,14 @@
-
-
-// Return the Length of the string after removing the balanced substring
-
 class Solution {
+
+    // Each removal eliminates 1 'a' and 1 'b', so the only characters left after full removals will be:
+    // the extra 'a' characters if countA > countB
+    // the extra 'b' characters if countB > countA
+    // So we have to return absolute count of A and B --> |countA-countB|
+    // Or we can say Math.max(A,B)-Math.min(A,B)
+
+
+    // Approach  - Counting Frequencies
+    // TC-O(n) and SC-O(1)
 
     // Count A and Count B
     public int minLengthAfterRemovals(String s) {
@@ -17,9 +23,7 @@ class Solution {
                 countB++;
         }
 
-        // abs (countA-countB)
-        // OR
-        // max(cA, cB)-min(cA, cB)
+        // abs(countA-countB) OR max(cA, cB)-min(cA, cB)
         return Math.max(countA, countB)-Math.min(countA, countB);
     }
 }
