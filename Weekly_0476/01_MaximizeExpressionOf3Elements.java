f5588661e22b56c8956799df3d880c885c2f8dc1
Ayush Jha <aj23.workspace@gmail.com>
2025-11-16 15:57:19 +0530

    Update 01_MaximizeExpressionOf3Elements.java

--- Weekly_0476/01_MaximizeExpressionOf3Elements.java
+++ Weekly_0476/01_MaximizeExpressionOf3Elements.java
@@ -1,9 +1,24 @@
 
-// Maximize the expression having 3 elements
+// Find a, b, c such that we have to return max(a+b−c)
+// choose max a and b, choose min c as it has to be subtracted
 
 class Solution {
 
-    // Linear
+    // Approach 1: Sort 
+    // TC-O(n log n) and SC-O(1)
+
+    // Sort the array:
+    // largest = nums[n-1]
+    // secondLargest = nums[n-2]
+    // smallest = nums[0]
+    // then return nums[n - 1] + nums[n - 2] - nums[0];
+
+
+    // Approach 2: Linear Scan
+    // TC-O(n) and SC-O(1)
+
+    // Find the largest, secondLargest and smallest
+    // return largest+secondLargest-smallest
     public int maximizeExpressionOfThree(int[] nums) {
         int n = nums.length;
         int largest = Integer.MIN_VALUE;
